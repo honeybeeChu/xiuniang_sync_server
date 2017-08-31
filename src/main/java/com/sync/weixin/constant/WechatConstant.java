5b2f4cc550ba0e9b1ba1e601bc4d10bd1f82e25c
chuliang <chuliang@LAPTOP-I7KH1T9O>
2017-08-31 22:02:08 +0800

    增加微信常量类

---
+++ src/main/java/com/sync/weixin/constant/WechatConstant.java
@@ -0,0 +1,8 @@
+package com.sync.weixin.constant;
+
+public class WechatConstant {
+	
+	//拉取会员信息接口
+	public static final String MEMBERCARD_USERINFO_GET = "https://api.weixin.qq.com/card/membercard/userinfo/get";
+
+}
